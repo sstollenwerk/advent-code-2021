9ef42b8dc462f5e0ab1c61ebd62d58837a6256f7
sstollenwerk <sstollenwerk@yahoo.co.nz>
2021-12-09 08:42:44 +1300

    create efficient implementation of day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -57,7 +57,137 @@ fn to_num(d: Vec<i32>) -> i32 {
     ans
 }
 
+fn group_by<T: std::clone::Clone + std::ops::Deref, V: std::hash::Hash + std::cmp::Eq + Copy>(
+    data: &Vec<T>,
+    key: impl Fn(&T) -> V,
+) -> HashMap<V, Vec<T>> {
+    let mut groups = HashMap::new();
+    for d in data.iter() {
+        let k = key(d);
+        let vals = groups.entry(k).or_insert(Vec::new());
+        // *vals.push(*d);
+        vals.push(d.clone());
+        let v = vals.to_vec();
+
+        groups.insert(k, v);
+    }
+
+    groups
+}
+
+fn flip<T: Copy, V: std::hash::Hash + std::cmp::Eq + Copy>(data: HashMap<T, V>) -> HashMap<V, T> {
+    let mut res = HashMap::new();
+
+    for (k, v) in data.iter() {
+        res.insert(*v, *k);
+    }
+
+    res
+}
+
+fn non_copy_partition<T: Copy>(
+    vals: Vec<HashSet<T>>,
+    cond: impl Fn(&HashSet<T>) -> bool,
+) -> (Vec<HashSet<T>>, Vec<HashSet<T>>) {
+    let mut t = Vec::new();
+    let mut f = Vec::new();
+    for v in vals.iter() {
+        if cond(&v) {
+            t.push(v.clone())
+        } else {
+            f.push(v.clone())
+        }
+    }
+
+    (t, f)
+}
+
 fn solve(parts: Row) -> i32 {
+    let (all_digits, check) = parts;
+    let all_digits: Digits = all_digits.into_iter().map(v_conv).collect();
+    let sizes_ = group_by(&all_digits, |x| x.len());
+    let mut sizes = HashMap::new();
+    for (k, v) in sizes_.iter() {
+        sizes.insert(
+            k,
+            v.iter()
+                .map(|s| s.iter().collect::<HashSet<_>>())
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    let mut knowns = HashMap::new();
+
+    knowns.insert(1, &sizes.get(&2).unwrap()[0]);
+    knowns.insert(7, &sizes.get(&3).unwrap()[0]);
+    knowns.insert(4, &sizes.get(&4).unwrap()[0]);
+    knowns.insert(8, &sizes.get(&7).unwrap()[0]);
+
+    let (others, sixes): (Vec<HashSet<_>>, Vec<HashSet<_>>) =
+        non_copy_partition(sizes.get(&6).unwrap().to_vec(), |s| {
+            s.is_superset(knowns.get(&1).unwrap())
+        });
+
+    knowns.insert(6, sixes.iter().next().unwrap());
+
+    let (nines, zeros): (Vec<HashSet<_>>, Vec<HashSet<_>>) =
+        non_copy_partition(others.to_vec(), |s| s.is_superset(knowns.get(&4).unwrap()));
+
+    knowns.insert(9, nines.iter().next().unwrap());
+    knowns.insert(0, zeros.iter().next().unwrap());
+
+    let (others, sixes): (Vec<HashSet<_>>, Vec<HashSet<_>>) =
+        non_copy_partition(sizes.get(&6).unwrap().to_vec(), |s| {
+            s.is_superset(knowns.get(&1).unwrap())
+        });
+
+    let (fives, others): (Vec<HashSet<_>>, Vec<HashSet<_>>) =
+        non_copy_partition(sizes.get(&5).unwrap().to_vec(), |s| {
+            s.is_subset(knowns.get(&6).unwrap())
+        });
+
+    knowns.insert(5, fives.iter().next().unwrap());
+
+    let (threes, twos): (Vec<HashSet<_>>, Vec<HashSet<_>>) =
+        non_copy_partition(others.to_vec(), |s| s.is_superset(knowns.get(&1).unwrap()));
+
+    knowns.insert(3, threes.iter().next().unwrap());
+    knowns.insert(2, twos.iter().next().unwrap());
+
+    let mut map_: HashMap<Vec<char>, i32> = HashMap::new();
+    for (k, v) in knowns.iter() {
+        map_.insert(v_conv(Vec::from_iter(v.iter().map(|x| **x))), *k);
+    }
+
+    to_num(
+        check
+            .iter()
+            .map(|x| *map_.get(&(x.to_vec())).unwrap())
+            .collect::<Vec<i32>>(),
+    )
+
+    /*
+    know
+    1,4,7,8
+
+
+    items with 6 elements are 0,6,9
+    1 subset of 0,9 . not 6
+    know 6
+    4 subset 9, not 0. can distingush
+
+    know 0,1,4,6,7,8,9
+
+
+    remaining are 2,3,5, all with 5 elements.
+    only 5 is a subset of 6, know 5
+
+    2,3 remain.
+    3 is superset of 1, 2 isn't
+    */
+}
+
+fn solve_ineff(parts: Row) -> i32 {
     let sections_ = HashMap::from([
         (0, "abcefg"),
         (1, "cf"),
