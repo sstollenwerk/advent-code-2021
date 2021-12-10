d445f6ad3aba614cea0c9befd5fba2e9f01c9195
sstollenwerk <sstollenwerk@yahoo.co.nz>
2021-12-10 18:34:47 +1300

    do day 9 part 2

--- src/day09.rs
+++ src/day09.rs
@@ -37,6 +37,20 @@ fn read_row(row: &str) -> Vec<u32> {
         .collect::<Vec<_>>()
 }
 
+fn adjacent_places(g: &Grid, pos: Place) -> Vec<Place> {
+    let nears = [
+        Complex::new(1, 0),
+        Complex::new(-1, 0),
+        Complex::new(0, 1),
+        Complex::new(0, -1),
+    ];
+    nears
+        .iter()
+        .map(|c| pos + c)
+        .filter(|c| g.contains_key(&c))
+        .collect()
+}
+
 fn adjacent(g: &Grid, pos: Place) -> Vec<Height> {
     let nears = [
         Complex::new(1, 0),
@@ -51,9 +65,62 @@ fn adjacent(g: &Grid, pos: Place) -> Vec<Height> {
         .map(|c| *c)
         .collect()
 }
+
+fn build_basin(g: &Grid, p: Place) -> HashSet<Place> {
+    let mut basin: HashSet<Place> = HashSet::new();
+
+    let mut to_check: HashSet<Place> = HashSet::new();
+    to_check.insert(p);
+
+    while !to_check.is_empty() {
+        let c = *to_check.iter().next().unwrap();
+        to_check.remove(&c);
+        basin.insert(c);
+        let nexts = adjacent_places(g, c)
+            .iter()
+            .map(|c| *c)
+            .filter(|c| *g.get(&c).unwrap() != 9)
+            .collect::<HashSet<Place>>()
+            .difference(&basin)
+            .map(|c| *c)
+            .collect::<HashSet<Place>>();
+        for n in nexts.iter() {
+            to_check.insert(*n);
+        }
+    }
+
+    basin
+}
+
+fn basins(g: &Grid) -> Vec<HashSet<Place>> {
+    let mut seen: HashSet<Place> = HashSet::new();
+    let mut groups = Vec::new();
+
+    for (k, v) in g.iter() {
+        let already_in = !seen.insert(*k);
+
+        if (already_in || *v == 9) {
+            continue;
+        }
+
+        let basin = build_basin(g, *k);
+        groups.push(basin.clone());
+        for s in basin.iter() {
+            seen.insert(*s);
+        }
+    }
+    groups
+}
+
+fn largests<T: Clone + std::cmp::Ord>(vals: &Vec<T>, n: u32) -> Vec<T> {
+    let mut v = vals.clone();
+    v.sort();
+    v.reverse();
+    v[..n as usize].to_vec()
+}
+
 pub fn part1() -> Height {
     let grid = get_data();
-    println!("{:?}", grid);
 
     let lows: Vec<Height> = grid
         .keys()
@@ -64,5 +131,11 @@ pub fn part1() -> Height {
 }
 
 pub fn part2() -> Height {
-    0
+    let basins = basins(&get_data());
+
+    let sizes = basins
+        .iter()
+        .map(|h| h.iter().count())
+        .collect::<Vec<usize>>();
+    largests(&sizes, 3).iter().product::<usize>() as Height
 }
