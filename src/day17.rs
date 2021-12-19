9c8a8c9c4acd67c0182f5b2c8ae353a241c41405
sstollenwerk <sstollenwerk@yahoo.co.nz>
2021-12-20 12:09:22 +1300

    Clean up day 17

--- src/day17.rs
+++ src/day17.rs
@@ -47,39 +47,13 @@ fn hit(pos: &Position, t: &Target) -> bool {
     ((x.0..=x.1).contains(&pos.re)) && ((y.0..=y.1).contains(&pos.im))
 }
 
-fn can_hit(pos: &Position, delta: &Position, t: &Target) -> bool {
-    let (x, y) = t;
-
-    !(pos.re > x.1) && ((pos.im >= y.0) || (delta.im >= 0))
-}
 fn adj_delta(delta: &Position) -> Position {
     let (x, y) = (delta.re, delta.im);
     Position::new(cmp::max(x - 1, 0), y - 1)
 }
 
-fn shoot(mut trajectory: Position, t: &Target) -> Option<Num> {
-    let mut pos = Position::new(0, 0);
-    let mut highest: Num = 0;
-    loop {
-        highest = cmp::max(highest, pos.im);
-        if hit(&pos, t) {
-            return Some(highest);
-        }
-        if !can_hit(&pos, &trajectory, t) {
-            break;
-        }
-        pos += trajectory;
-        trajectory = adj_delta(&trajectory);
-    }
-    return None;
-}
-
 fn y_at_step(y: Num, step: Num) -> Num {
     y * (step + 1) - (step * (step + 1)) / 2
-}
-fn limit(x:Num) -> Num {
-    (x * (x + 1)) / 2
-
 }
 
 fn x_could_reach(t: &Target) -> HashMap<Num, HashSet<Num>> {
@@ -91,11 +65,11 @@ fn x_could_reach(t: &Target) -> HashMap<Num, HashSet<Num>> {
     for x in (lower_limit..=t.0 .1) {
         let mut pos = Position::new(0, 0);
         let mut vel = Position::new(x, 0);
-        for i in (0.. x + t.0.0.abs()*  10 ) {
+        for i in (0..x + t.0 .0.abs() * 3) {
             pos += vel;
             vel = adj_delta(&vel);
             if hit(&pos, &t_) {
-                let mut vals = res.entry(x).or_insert(HashSet::new());
+                let vals = res.entry(x).or_insert(HashSet::new());
                 vals.insert(i);
             }
         }
@@ -103,29 +77,6 @@ fn x_could_reach(t: &Target) -> HashMap<Num, HashSet<Num>> {
 
     res
 }
-fn all_y_could_reach(t: &Target) -> HashMap<Num, HashSet<Num>> {
-    let (x_, y_) = t;
-    let t_ = ((Num::MIN, 0), *y_);
-    let lower_limit = cmp::min(0, y_.0);
-    let upper_limit = cmp::max(0, y_.1.abs() );
-    println!("{:?}", (lower_limit, upper_limit));
-
-    let mut res = HashMap::new();
-    for y in (lower_limit..=upper_limit) {
-        let mut pos = Position::new(0, 0);
-        let mut vel = Position::new(0, y);
-        for i in (0..upper_limit*2) {
-            pos += vel;
-            vel = adj_delta(&vel);
-            if hit(&pos, &t_) {
-                let mut vals = res.entry(y).or_insert(HashSet::new());
-                vals.insert(i);
-            }
-        }
-    }
-
-    res}
-
 
 fn y_could_reach(t: &Target, step: Num) -> HashSet<Num> {
     let (x_, y_) = t;
@@ -133,7 +84,7 @@ fn y_could_reach(t: &Target, step: Num) -> HashSet<Num> {
 
     let lower_limit = cmp::min(0, y_.0);
     let mut res = HashSet::new();
-    for y in (lower_limit..=cmp::max(step, y_.1.abs() )*10) {
+    for y in (lower_limit..=cmp::max(step, y_.1.abs()) * 10) {
         let pos = Position::new(0, y_at_step(y, step));
 
         if hit(&pos, &t_) {
@@ -145,53 +96,41 @@ fn y_could_reach(t: &Target, step: Num) -> HashSet<Num> {
 }
 
 fn highest_shot(t: &Target) -> Num {
-    let mut heights: HashSet<Num>  = HashSet::new();
-    for step in x_could_reach(&t).into_values().flatten().collect::<HashSet<Num>>().into_iter() { 
-
-       let  ys = y_could_reach(t, step );
+    let mut heights: HashSet<Num> = HashSet::new();
+    for step in x_could_reach(&t)
+        .into_values()
+        .flatten()
+        .collect::<HashSet<Num>>()
+        .into_iter()
+    {
+        let ys = y_could_reach(t, step);
         for y in ys {
-
-            heights.insert(y_at_step( y, y ) );
+            heights.insert(y_at_step(y, y));
         }
-
     }
     heights.into_iter().max().unwrap()
 }
 
 fn num_shots(t: &Target) -> Num {
-    let mut shots: HashSet<Position>  = HashSet::new();
-  for (x, steps) in x_could_reach(&t).iter() { 
-  //      for (x, steps) in HashMap::from([ (6, vec![4,5] ) ]) .iter() {
-             
-            for step in steps.into_iter() {
-
-            for y in y_could_reach(t, *step ) {
-                shots.insert(    Position::new(*x,y));
-            } 
+    let mut shots: HashSet<Position> = HashSet::new();
+    for (x, steps) in x_could_reach(&t).iter() {
+        for step in steps.into_iter() {
+            for y in y_could_reach(t, *step) {
+                shots.insert(Position::new(*x, y));
+            }
         }
     }
- //   shots.sort_by_key(|c| (c.re, c.im) );
-
-   // println!("{:?}", shots);
 
     shots.len() as Num
-
 }
 
 pub fn part1() -> Num {
     let target = get_data();
 
-    let x = x_could_reach(&target);
-    println!("{:?}", x);
-
-
     highest_shot(&target)
-
 }
 pub fn part2() -> Num {
     let target = get_data();
 
-    let y = all_y_could_reach(&target);
-    println!("{:?}", y);
     num_shots(&target)
 }
